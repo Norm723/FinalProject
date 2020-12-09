d0f8920b9023b3c8f992702106628ae86eefa1f9
norm723 <norm.twersky@gmail.com>
2020-12-09 22:14:10 +0200

    dataset works

--- DataSet.py
+++ DataSet.py
@@ -5,8 +5,8 @@ import numpy as np
 def getMeans(temp):
     length = (len(temp)-1)
     temp_avg = np.zeros(length)
-    for i in length:
-        temp_avg[i] = np.mean(temp[i:i+1])
+    for i in range(length):
+        temp_avg[i] = np.mean(temp[i:i+2])
     return temp_avg
 
 
@@ -14,23 +14,32 @@ class DataSet:
     def __init__(self, file_path=None):
         if file_path is not None:
             # check if path is good
+            # automatically removes first row
             self.data = pd.read_csv(file_path)
             self.data = self.data.to_numpy()
         else:
             exit
 
     def getThresholds(self, index):
-        temp = np.apply_over_axes(np.sort, axes=0, a=self.data)
-        temp = temp.transpose()
-        temp = temp[index]
-        temp = np.unique(temp)
-        return getMeans(temp)
+        temp = self.data
+        temp2 = list()
+        for i in range(len(temp)):
+            temp2.append(temp[i][index])
+        temp2 = np.unique(temp2)
+        temp2 = np.sort(temp2)
+        return getMeans(temp2)
 
+    # returns all possible thresholds as a list of tuples with (value, 0)
+    # 0 is starting pheromone value for ACO
     def getAllThresholds(self):
         len = self.data.shape[1] - 1
         threshes = list()
         for i in range(len):
-            threshes.append(self.getThresholds(i))
+            temp = self.getThresholds(i)
+            tempTouples = list()
+            for j in range(temp.size):
+                tempTouples.append((temp[j], 0))
+            threshes.append(tempTouples)
         return threshes
 
     def printData(self):
--- test.py
+++ test.py
@@ -1,7 +1,29 @@
 import pandas as pd
 import numpy as np
 import os
+import DataSet
 
-temp = pd.read_csv("C:\\Users\\normt\\Desktop\\semester6\\bigData\\project\\Batting.csv")
-temp = temp.to_numpy()
-giniImpurity(temp)
+
+# giniImpurity(temp)
+
+# temp = np.array([2,33,4,5,6,7,7,8,9,10])
+# lengthTemp = len(temp)
+# tempTouples = list()
+# for j in range(lengthTemp):
+#     tempTouples.append((temp[j], 0))
+
+# print(tempTouples[2][0])
+
+# temp = pd.read_csv("C:\\Users\\normt\\Desktop\\semester6\\bigData\\project\\Batting.csv")
+# temp = temp.to_numpy()
+# temp2 = list()
+# for i in range(len(temp)):
+#     temp2.append(temp[i][6])
+# temp2 = np.unique(temp2)
+# temp2 = np.sort(temp2)
+# temp2 = DataSet.getMeans(temp2)
+# print(temp2)
+
+ds = DataSet.DataSet("C:\\Users\\normt\\Desktop\\testData.csv")
+f = ds.getAllThresholds()
+print(f[0][0][0])
